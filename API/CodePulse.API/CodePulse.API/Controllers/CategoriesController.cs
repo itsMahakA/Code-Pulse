d942cfab71674071341e414ba491b9c15e113121
Mahak Agrawal <mahakagrawalknw10@gmail.com>
2023-10-07 22:53:54 +0530

    First commit

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        // private readonly ApplicationDbContext dbContext;
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+            //this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDTO request)
+        {
+            //DTO to domain CreateCategoryRequestDTO
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            //Domain to DTO CategoryDTO
+            var response = new CategoryDTO
+            {
+                Id = category.Id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+            return Ok(response);
+        }
+    }
+}
